86400f48b05f32f8efecc890d12a58670a5b2cce
Scott Robbin <scott@robbin.co>
2017-12-16 15:05:00 -0600

    Draft of `new` command

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vapid CLI
+A command line iterface for Vapid, a template-driven content management system.
--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 
-const pjson = require('../package.json')
 const program = require('commander')
+
+const pjson = require('../package.json')
+const Site = require('../lib/site')
+const Logger = require('../lib/logger')
 
 // NEW
 program
-  .command('new <target_dir>')
+  .command('new <target>')
   .description('create a new project')
   .action((target) => {
-    console.log(`NEW: ${target}`)
+    let site = new Site(target)
+
+    try {
+      site.localInitialize()
+      Logger.info(`Project created.`)
+      Logger.extra([
+        'To start the development server now, run:',
+        `  ${pjson.name} server ${target}`
+      ])
+    } catch (err) {
+      Logger.error(err)
+    }
   })
 
 // SERVER
@@ -16,7 +30,13 @@ program
   .command('server')
   .description('start the development server')
   .action((target) => {
-    console.log(`SERVER`)
+    // let server = new Server(target)
+    
+    Logger.info(`Starting the development server...`)
+    Logger.extra([
+      'View your site at http://localhost:4567',
+      'Ctrl + C to quit'
+    ])
   })
 
 // DEPLOY
@@ -24,7 +44,7 @@ program
   .command('deploy')
   .description('deploy to Vapid\'s hosting service')
   .action((target) => {
-    console.log(`DEPLOY`)
+    Logger.info(`DEPLOY`)
   })
 
 // VERSION
@@ -32,13 +52,13 @@ program
   .command('version')
   .description('shows the version number')
   .action((target) => {
-    console.log(`Vapid ${program.version()}`)
+    Logger.extra(`Vapid ${program.version()}`)
   })
 
-if (!process.argv.slice(2).length) {
+if (process.argv.slice(2).length) {
+  program
+    .version(pjson.version)
+    .parse(process.argv)
+} else {
   program.help()
 }
-
-program
-  .version(pjson.version)
-  .parse(process.argv)
