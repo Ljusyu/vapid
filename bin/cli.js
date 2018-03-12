379a104d0b521d7e9db03feaffd8fe9252cc74e6
Scott Robbin <scott@robbin.co>
2018-03-12 10:39:28 -0500

    WIP refactor with root Vapid class

--- bin/cli.js
+++ bin/cli.js
@@ -2,24 +2,18 @@
 
 const path = require('path')
 const program = require('commander')
-const repl = require('repl')
 
-const env = process.env.NODE_ENV || 'development'
 const pjson = require('../package.json')
-const Console = require('../lib/console')
-const Server = require('../lib/server')
-const Site = require('../lib/site')
-const Logger = require('../lib/logger')
+const Vapid = require('../lib/vapid')
 
 function actionHandler(fn) {
   return (target) => {
-    let sitePath = typeof target == 'string' ? target : '.'
-    let site = new Site(sitePath)
+    global.vapid = new Vapid(target)
 
     try {
-      fn(site)
+      fn(target)
     } catch (err) {
-      Logger.error(err)
+      vapid.log.error(err)
     }
   }
 }
@@ -28,12 +22,12 @@ function actionHandler(fn) {
 program
   .command('new <target>')
   .description('create a new project')
-  .action(actionHandler(site => {
-    site.localInitialize()
-    Logger.info(`Project created.`)
-    Logger.extra([
+  .action(actionHandler(target => {
+    vapid.initSite()
+    vapid.log.info('Site created.')
+    vapid.log.extra([
       'To start the development server now, run:',
-      `  ${pjson.name} server ${site.localPath}`
+      `  ${pjson.name} server ${target}`
     ])
   }))
 
@@ -42,54 +36,29 @@ program
   .command('server')
   .description('start the server')
   .action(actionHandler(site => {
-    // TODO: Options for livereload, port, bind, etc?
-    const server = new Server(site, { livereload: env == 'development' })
-     
-    Logger.info(`Starting the ${env} server...`)
-    server.start()
-    Logger.extra([
-      `View your site at http://localhost:${server.port}`,
+    vapid.log.info(`Starting the ${vapid.env} server...`)
+    vapid.startServer()
+    vapid.log.extra([
+      `View your site at http://localhost:${vapid.server.port}`,
       'Ctrl + C to quit'
     ]);
-  }))
-
-// CONSOLE
-program
-  .command('console')
-  .description('start the interactive console')
-  .action(actionHandler(site => {
-    Logger.info(`Loading the ${env} environment`)
-    Logger.extra([
-      'Type .exit to quit'
-    ]);
-
-    const c = new Console(site)
-    c.start()
-  }))
-
-// DEPLOY
-program
-  .command('deploy')
-  .description('deploy to Vapid\'s hosting service')
-  .action(actionHandler(site => {
-    Logger.info(`DEPLOY`)
   }))
 
 // VERSION
 program
   .command('version')
   .description('shows the version number')
-  .action(target => {
-    Logger.extra(`Vapid ${program.version()}`)
-  })
+  .action(actionHandler(target => {
+    vapid.log.extra(`Vapid ${program.version()}`)
+  }))
 
 // CATCH-ALL
 program
   .command('*', { noHelp: true })
-  .action(() => {
-    Logger.error(`Command "${process.argv[2]}" not found.`)
+  .action(actionHandler(target => {
+    vapid.log.error(`Command "${process.argv[2]}" not found.`)
     program.help()
-  })
+  }))
 
 if (process.argv.slice(2).length) {
   program
