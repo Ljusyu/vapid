2677f89a05567d39057bca38f43415aaae089bb7
Scott Robbin <scott@robbin.co>
2018-03-21 15:38:20 -0500

    Fixed ACE Editor bug

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -1,8 +1,8 @@
-var editor = ace.edit('editor');
 var $editor = document.querySelector('#editor')
 var $textarea = document.querySelector('textarea[name^=content]')
 
 if ($editor) {
+  var editor = ace.edit('editor');
   editor.getSession().setValue($textarea.value)
 
   editor.getSession().on("change", function () {
