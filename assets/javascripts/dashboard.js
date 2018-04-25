75f52e3bb585ffe79da5309aa3cc88f53b1bec57
Scott Robbin <scott@robbin.co>
2018-04-25 11:02:46 -0500

    Linting

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -1,41 +1,41 @@
-$(document).ready(function () {
+$(document).ready(() => {
   // Semantic UI
-  $('.ui.checkbox').checkbox()
-  $('.ui.dropdown').dropdown()
+  $('.ui.checkbox').checkbox();
+  $('.ui.dropdown').dropdown();
 
   // Sidebar
-  $('[data-toggle=sidebar]').click(function (e) {
-    e.preventDefault()
-    $('.ui.sidebar').sidebar('toggle')
-  })
+  $('[data-toggle=sidebar]').click((e) => {
+    e.preventDefault();
+    $('.ui.sidebar').sidebar('toggle');
+  });
 
   // Ace Editor
   $('.ace_editor').each(function () {
-    const $editor = $(this)
-    const $textarea = $editor.next("textarea")
+    const $editor = $(this);
+    const $textarea = $editor.next('textarea');
 
     editor = ace.edit(this);
-    editor.getSession().setValue($textarea.val())
+    editor.getSession().setValue($textarea.val());
 
-    editor.getSession().on("change", function () {
-      $textarea.val( editor.getSession().getValue() )
-    })
+    editor.getSession().on('change', () => {
+      $textarea.val(editor.getSession().getValue());
+    });
 
-    $editor.height(300)
-    $textarea.hide()
-  })
+    $editor.height(300);
+    $textarea.hide();
+  });
 
   // Hide messages
-  $(".ui.form").submit(function () {
-    $(".ui.message").hide()
-  })
+  $('.ui.form').submit(() => {
+    $('.ui.message').hide();
+  });
 
   // WebSocket listener
-  if ("WebSocket" in window) {
-    const ws = new WebSocket("ws://localhost:3000/livereload")
+  if ('WebSocket' in window) {
+    const ws = new WebSocket('ws://localhost:3000/livereload');
 
     ws.onmessage = (evt) => {
-      console.log(JSON.parse(evt.data))
-    } 
+      console.log(JSON.parse(evt.data));
+    };
   }
-})
+});
--- bin/cli.js
+++ bin/cli.js
@@ -1,104 +1,107 @@
 #!/usr/bin/env node
 
-const dotenv = require('dotenv')
-const { resolve } = require('path')
-const program = require('commander')
+const dotenv = require('dotenv');
+const { resolve } = require('path');
+const program = require('commander');
+const childProcess = require('child_process');
 
-const { version } = require('../package.json')
-const Logger = require('../lib/logger')
-const Vapid = require('../lib/vapid')
+const { version } = require('../package.json');
+const Logger = require('../lib/logger');
+const Vapid = require('../lib/vapid');
 
 function actionHandler(fn, requirePJSON = true) {
-  return (target) => {
-    let opts
+  return (cwd) => {
+    let opts;
 
     try {
-      target = target instanceof program.Command ? process.cwd() : target
-      opts = requirePJSON && require(resolve(target, 'package.json')).vapid || {}
+      const target = cwd instanceof program.Command ? process.cwd() : cwd;
+      /* eslint-disable-next-line global-require, import/no-dynamic-require */
+      opts = (requirePJSON && require(resolve(target, 'package.json')).vapid) || {};
 
-      dotenv.config({ path: resolve(target, '.env') })
-      global.vapid = new Vapid(target, opts)
+      dotenv.config({ path: resolve(target, '.env') });
+      global.vapid = new Vapid(target, opts);
 
-      fn(target)
+      fn(target);
     } catch (err) {
-      Logger.error(err)
+      Logger.error(err);
     }
-  }
+  };
 }
 
 // NEW
 program
   .command('new <target>')
   .description('create a new site')
-  .action(actionHandler(target => {
-    vapid.initSite()
-    vapid.log.info('Site created.')
+  .action(actionHandler((target) => {
+    vapid.initSite();
+    vapid.log.info('Site created.');
     vapid.log.extra([
       'To start the development server now, run:',
-      `  vapid-cli server ${target}`
-    ])
-  }, false))
+      `  vapid-cli server ${target}`,
+    ]);
+  }, false));
 
 // SERVER
 program
   .command('server')
   .description('start the server')
-  .action(actionHandler(async target => {
-    vapid.log.info(`Starting the ${vapid.env} server...`)
-    await vapid.startServer()
+  .action(actionHandler(async () => {
+    vapid.log.info(`Starting the ${vapid.env} server...`);
+    await vapid.startServer();
     vapid.log.extra([
       `View your site at http://localhost:${vapid.config.port}`,
-      'Ctrl + C to quit'
+      'Ctrl + C to quit',
     ]);
-  }))
+  }));
 
 // DEPLOY
 program
   .command('deploy')
   .description('deploy to Vapid\'s hosting service')
-  .action(actionHandler(target => {
-    let tjson = require(resolve(target, 'package.json'))
-    let deploy = tjson.scripts.deploy
+  .action(actionHandler((target) => {
+    /* eslint-disable-next-line global-require, import/no-dynamic-require */
+    const tjson = require(resolve(target, 'package.json'));
+    const { deploy } = tjson.scripts;
 
-    vapid.log.info("Deploying...")
-    
+    vapid.log.info('Deploying...');
+
     if (deploy) {
-      require('child_process').exec(deploy, (err, stdout, stderr) => {
+      childProcess.exec(deploy, (err, stdout, stderr) => {
         if (err) {
-          vapid.log.error(stderr)
-          return
+          vapid.log.error(stderr);
+          return;
         }
 
-        vapid.log.extra(stdout)
-      })
+        vapid.log.extra(stdout);
+      });
     } else {
       vapid.log.extra([
-        `Vapid hosting is currently in private beta.`,
-        'To request access, visit https://www.vapid.com'
+        'Vapid hosting is currently in private beta.',
+        'To request access, visit https://www.vapid.com',
       ]);
     }
-  }))
+  }));
 
 // VERSION
 program
   .command('version')
   .description('shows the version number')
   .action(() => {
-    Logger.extra(`Vapid ${program.version()}`)
-  })
+    Logger.extra(`Vapid ${program.version()}`);
+  });
 
 // CATCH-ALL
 program
   .command('*', { noHelp: true })
   .action(() => {
-    Logger.error(`Command "${process.argv[2]}" not found.`)
-    program.help()
-  })
+    Logger.error(`Command "${process.argv[2]}" not found.`);
+    program.help();
+  });
 
 if (process.argv.slice(2).length) {
   program
     .version(version)
-    .parse(process.argv)
+    .parse(process.argv);
 } else {
-  program.help()
+  program.help();
 }
